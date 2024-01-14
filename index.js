06705f33f14855c69bedad7d893ba11e7631ea5c
collinsbett023 <collinsbett023@proton.me>
2024-01-14 15:40:31 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function saturdayFun(activity = "roller-skate"){
     console.log(`This Saturday, I want to ${activity}!`)
 }
 
-function mondayWork(activity = "go to the office."){
+const mondayWork = function(activity = "go to the office."){
     console.log(`This Monday, I will ${activity}`)
 }
 
